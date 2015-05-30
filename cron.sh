594dc60b0d194047d401ddbab42f43df47f7e174
Jonathan Delorme <delorme.jonathan@gmail.com>
2015-05-30 20:14:18 +0200

    Fixed database path.

--- cron.sh
+++ cron.sh
@@ -1,8 +1,9 @@
 #/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-ids=$(sqlite3 app/database/production.sqlite "SELECT id FROM shares WHERE expiration > date('now')")
+ids=$(sqlite3 $DIR/app/database/production.sqlite "SELECT id FROM shares WHERE expiration < date('now')")
 for id in $ids
 do
-	sqlite3 app/database/production.sqlite "DELETE FROM shares WHERE id=$id"
-	sqlite3 app/database/production.sqlite "DELETE FROM photos WHERE share_id=$id"
+	sqlite3 $DIR/app/database/production.sqlite "DELETE FROM shares WHERE id=$id"
+	sqlite3 $DIR/app/database/production.sqlite "DELETE FROM photos WHERE share_id=$id"
 done
